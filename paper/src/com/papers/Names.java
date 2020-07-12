19e658e249e010d293cfc92d209aa0b98f41690b
naresh <nareshnalla567@gmail.com>
2020-07-12 07:39:46 +0530

    ok

---
+++ paper/src/com/papers/Names.java
@@ -0,0 +1,22 @@
+package com.papers;
+
+public class Names {
+	public void add(String name) {
+		
+		
+	}
+public static void main(String[] args) {
+	
+	Names name1=new Names();
+	name1.add("enadu");
+	
+	System.out.println("andhrapapers");
+	
+	
+	
+
+
+
+
+}
+}
